daa050ae7e227bd2a8a9051fed4eb5447d4fdc2f
Ian Ernest Voysey <iev@andrew.cmu.edu>
2014-09-06 15:55:59 -0400

    three new things:
    1) recipes all exist in a section under the chapter called "recipies". this
       fixes some warning messages and makes the book marker utility bitch
       less.
    
    2) the perl script now recognizes a "discuss.txt" file in each
       subdirectory. if such a thing exists, it dumps the contents into a
       "Discussion" section at the top of the chapter. this should contain
       information that is specific to the chapter but not to any recipe there
       in. this seems like something that we might want and want to expand over
       time, actually. the sticky bit is that this current solution is
       hardcoded and not really extensible, in that you can't just add more
       *.txt files and have the script pick them up. to do that, i'll need to
       actually bother to write a smarter script rather than relying soley on
       the most immediately obvious thing that File::Find can do.
    
    3) the perl script inserts dummy sections in front of every god damn
       recipe. this means that clicking on things in the ToC, either in the PDF
       or in the metadata, will actually jump you to the thing you clicked
       instead of the top of the chapter that contains the thing you clicked.

--- list.pl
+++ list.pl
@@ -5,19 +5,29 @@ use File::Find;
 ## todo : make aware of subsections?
 
 sub caps{
-    my $line = (split(/\//, shift))[-1];
-    $line =~ s/_/ /g;
-    $line =~ s/(\w+)/\u\L$1/g;
-    return $line;
+  my $line = (split(/\//, shift))[-1];
+  $line =~ s/_/ /g;
+  $line =~ s/(\w+)/\u\L$1/g;
+  return $line;
 }
 
 sub wanted{
-    if(-f && m/\.tex$/){
-	print "\\input{$_}\n";
+  if(-f && m/\.tex$/){
+    print "\\section*{}\n";
+    print "\\input{$_}\n";
+  }
+
+  if(-d) {
+    print "\\chapter{" . caps($_) . "}\n";
+
+    if (-f "$_/discuss.txt"){
+      print "\\section{Discussion}\n";
+      print "\\input{$_/discuss.txt}\n";
     }
-    if(-d) {
-	print "\\chapter{" . caps($_) . "}\n";
-    }
+
+    print "\\section{Recipies}\n";
+
+  }
 }
 
 find({ wanted => \&wanted, no_chdir => 1 }, <recipes/*>);
